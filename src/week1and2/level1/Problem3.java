70d4df984c8add642bff7a9d8a78710c10a77128
Riddhi Sharan <sharanriddhi28@gmail.com>
2026-03-03 20:19:34 +0530

    Level 1 Completed

---
+++ src/week1and2/level1/Problem3.java
@@ -0,0 +1,9 @@
+package week1and2.level1;
+
+public class Problem3 {
+ public static void main(String[] args) {
+        double dist_in_km =10.8;
+        double dist_in_miles = dist_in_km * 1.6;
+        System.out.println("The distance " + dist_in_km + " km is equal to " + dist_in_miles + " miles.");
+    }
+}
