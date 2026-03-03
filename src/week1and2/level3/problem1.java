38587ddcb4d0882bcd123b21b7a6d34349f5e909
Riddhi Sharan <sharanriddhi28@gmail.com>
2026-03-03 20:21:13 +0530

    Level 3 Completed

---
+++ src/week1and2/level3/problem1.java
@@ -0,0 +1,18 @@
+package week1and2.level3;
+
+import java.util.Scanner;
+
+public class problem1 {
+public static void main(String[] args) {
+        System.out.println("Number of Chocolates: ");
+        Scanner sc = new Scanner(System.in);
+        int chocolates = sc.nextInt();
+        sc.close();
+        System.out.println("Number of Friends: ");
+        int friends = sc.nextInt();
+        
+        int remainingChocolates = chocolates % friends;
+        int chocolatePerPerson = chocolates / friends;
+        System.out.println("Each friend gets " + chocolatePerPerson + " chocolates and " + remainingChocolates + " chocolates are left over.");
+    }
+}
